7aa7635b16eddc8fa53b42da2c2246c3fb0c5e5c
Warren Hansen <whansen1@mac.com>
2017-10-31 13:36:34 -0700

    task: added tableview

--- Channel System/SymbolsViewController.swift
+++ Channel System/SymbolsViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class SymbolsViewController: UIViewController {
+class SymbolsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titleArray = [String]()
     
     var dataFeed = DataFeed()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,9 +23,23 @@ class SymbolsViewController: UIViewController {
         for prices in self.dataFeed.sortedPrices {
             print("\(prices.date!)\t\(prices.ticker!)\to:\(prices.open!)\th:\(prices.high!)\tL:\(prices.low!)\tC:\(prices.close!)")
         }
+        titleArray.append((self.dataFeed.sortedPrices.last?.ticker)!)
     }
-
-
+    
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        // for custom cell
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as!StockTableViewCell
+        cell.textLabel?.text = titleArray[indexPath.row]
+        
+        return cell
+    }
     
 
 
