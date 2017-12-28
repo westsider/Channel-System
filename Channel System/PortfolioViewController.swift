1179c28722621d2e2c89201eaee81aa0f34ea836
Warren Hansen <whansen1@mac.com>
2017-12-28 11:06:06 -0600

    task: portfolio now showing correct P&L

--- Channel System/PortfolioViewController.swift
+++ Channel System/PortfolioViewController.swift
@@ -39,7 +39,7 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     func activateButton(bool: Bool)-> (String, UIColor, UIColor) {
         isOn = bool
         let onColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        let offColor = #colorLiteral(red: 0.3489862084, green: 0.3490410447, blue: 0.3489741683, alpha: 1)
+        let offColor = #colorLiteral(red: 0.3489862084, green: 0.3490410447, blue: 0.3489741683, alpha: 0)
         let onTitle = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         let offTitle = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         let color = bool ? onColor : offColor
@@ -60,11 +60,13 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let date = tasks[indexPath.row].dateString
         let shortDate = date.dropFirst(5)
-        let task:String = "\(shortDate) \t\(tasks[indexPath.row].ticker)"
+        let thisSymbol = Prices().sortOneTicker(ticker: tasks[indexPath.row].ticker, debug: false).last
+        let closeString = String(format: "%.2f", (thisSymbol?.close)!)
+        let task:String = "\(shortDate) \t\(tasks[indexPath.row].ticker) \t\(closeString)"
         cell.textLabel?.text = task
        
-        let profit:Double = (tasks[indexPath.row].entry - tasks[indexPath.row].close) * Double(tasks[indexPath.row].shares)
-         print("\nHere is the entry price \(tasks[indexPath.row].entry) and close \(tasks[indexPath.row].close) shares \(tasks[indexPath.row].shares) and profit \(profit)")
+        let profit:Double = (thisSymbol!.close - tasks[indexPath.row].entry) * Double(tasks[indexPath.row].shares)
+        print("\n\(tasks[indexPath.row].ticker) entry: \(tasks[indexPath.row].entry) and close \(thisSymbol!.close) shares \(tasks[indexPath.row].shares) and profit \(profit)")
         cell.detailTextLabel?.text = (String(format: "%.2f", profit))
         
         if profit < 0 {
@@ -76,7 +78,8 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "$\(totalOpenProfit().0) profit \t\(totalOpenProfit().1)% win"
+        let openProfit = totalOpenProfit()
+        return "$\(openProfit.0) profit \t\(openProfit.1)% win"
     }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int){
@@ -90,7 +93,9 @@ class PortfolioViewController: UIViewController, UITableViewDataSource, UITableV
         var sum = 0.00
         var wins = 0.00
         for each in tasks {
-            let profit:Double = (each.entry - each.close) * Double(each.shares)
+            let thisSymbol = Prices().sortOneTicker(ticker: each.ticker, debug: false).last
+            let profit:Double = (thisSymbol!.close - each.entry ) * Double(each.shares)
+            print("\(each.ticker) profit: \(String(format: "%.2f", profit)) = c:\(thisSymbol!.close) - e:\(each.entry) * s:\(each.shares)")
             sum += profit
             if profit > 0 {
                 wins += 1
